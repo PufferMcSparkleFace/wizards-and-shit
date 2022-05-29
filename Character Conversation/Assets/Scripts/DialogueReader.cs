5306387595051eafa0591d1fe2a33e81cc2b7a07
Daniel Gutierrez <83122636+PufferMcSparkleFace@users.noreply.github.com>
2022-05-29 23:31:18 +1200

    troubleshooting

--- Character Conversation/Assets/Scripts/DialogueReader.cs
+++ Character Conversation/Assets/Scripts/DialogueReader.cs
@@ -22,7 +22,7 @@ public class DialogueReader : MonoBehaviour
 
     void Update()
     {
-        if (!Input.GetKeyDown(KeyCode.Space))
+        if (!Input.GetKeyDown(KeyCode.Space) || choicesPanel.activeInHierarchy)
         {
             return;
         }
